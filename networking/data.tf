7bace24e0bf15cb4381bbcc6dd0e394364e0e34d
kvendingoldo <kvendingoldo@yandex.ru>
2022-08-06 21:43:00 +0400

    feat: update networking module (#10)

--- networking/data.tf
+++
@@ -1,3 +0,0 @@
-data "openstack_networking_network_v2" "extnet" {
-  name = "ext-net"
-}
---
+++ vkcs/networking/variables.tf
@@ -0,0 +1,28 @@
+variable "blank_name" {
+  description = "Blank name which will be used for network & route name"
+  type        = string
+}
+
+variable "subnets" {
+  description = "List and specification of network's subnets"
+  type        = list(object(
+    {
+      name            = string
+      cidr_block      = string
+      ip_version      = string
+      dns_nameservers = list(any)
+    }
+  ))
+}
+
+variable "external" {
+  description = "The external routing facility of the network"
+  type        = bool
+  default     = false
+}
+
+variable "vkcs_services_access" {
+  description = "Whether VKCS services access is enabled"
+  type        = bool
+  default     = true
+}
