df24f3c829ad0f9895e2075944bd714369bb0421
Nikolai Mishin <sanduku.default@gmail.com>
2022-09-12 10:41:00 +0200

    Fix output of vm_subnet_id
    vm_subnet_id should output id, but prevoiusly it shows all info about
    subnet.

--- vkcs/networking/outputs.tf
+++ vkcs/networking/outputs.tf
@@ -6,7 +6,6 @@ output "network_name" {
   value = vkcs_networking_network.main.name
 }
 
-# TODO: make output "name"="id"
-output "subnet_ids" {
-  value = vkcs_networking_subnet.main
+output "subnet_id" {
+  value = vkcs_networking_subnet.main.id
 }
